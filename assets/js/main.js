30a3bdcfe06a1caf2171f431cddce8826ad982d4
odify <odify495@gmail.com>
2020-05-18 22:46:04 +0200

    final-update-1

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+// CluedoJS Array...
+
+let roomsArray = ["Conservatory ", "Dining Room ", "Kitchen ", "Guest House ", "Patio", "Ballroom", "Library ", "Billiard Room "];
+
+let weaponsArray = [" pistol", "trophy", "knife", "dumbbell ", "bat", "axe ", "poison ", "rope ", "candlestick "];
+
+let suspectsArray = [
+    {
+        Name: 'mrGreen ',
+        age: 34,
+        occupation: 'Football player ',
+    },
+    {
+        Name: 'drOrchid',
+        age: 31,
+        occupation: 'Scientist',
+    },
+    {
+        Name: 'profPlum',
+        age: 54,
+        occupation: 'Actor',
+    },
+    {
+        Name: 'missScarlet',
+        age: 32,
+        occupation: 'Designer',
+    },
+    {
+        Name: 'mrsPeacock',
+        age: 29,
+        occupation: 'programer',
+    },
+    {
+        Name: 'mrMustard ',
+        age: 43,
+        occupation: 'Entrepreneur',
+    },
+
+];
+
+
+let random1, random2, random3;
+
+
+randomNum = () => {
+    random1 = Math.floor(Math.random() * 8);
+    random2 = Math.floor(Math.random() * 9);
+    random3 = Math.floor(Math.random() * 6);
+}
+
+
+document.getElementById("check").addEventListener("click", () => {
+
+    randomNum();
+
+    console.log(random3);
+
+    document.getElementById("show").innerHTML = ` Who ?: ${suspectsArray[random3].Name}<br> Where ?: ${roomsArray[random1]}<br> Weapon: ${weaponsArray[random2]} <br>` + `<img src="assets/img/${random3}.jpg" alt="">`;
+
+})
